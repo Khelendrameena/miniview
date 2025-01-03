23d7e9386e0d4886b7f7f042affcdea64581e755
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-03 18:32:50 +0530

    Update settings.py

--- newswatch/settings.py
+++ newswatch/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles'
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount'
 ]
 
 ACCOUNT_EMAIL_REQUIRED = True
