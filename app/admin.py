8b4137cbdb5f3660df59c8c90cab590fa48d3177
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-04 19:22:19 +0530

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -5,11 +5,9 @@ from app.models import comentconfig
 from app.models import Profile
 from app.models import Vlog
 from app.models import UserReaction
-from app.models import param
 
 admin.site.register(MyModel)
 admin.site.register(comentconfig)
 admin.site.register(Profile)
 admin.site.register(UserReaction)
 admin.site.register(Vlog)
-admin.site.register(param)
