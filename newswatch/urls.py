64a0ed38dd941c79d3bc01f46c317f74bde1f3e6
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-06 17:53:15 +0530

    Update urls.py

--- newswatch/urls.py
+++ newswatch/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from app.views import google_login, google_callback
+from django.conf.urls.static import static
 #from django.conf.urls import handler500
 
 #handler500 = 'myapp.views.custom_500_error'
