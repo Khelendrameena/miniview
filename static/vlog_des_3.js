e677ffa91150257fc6de7cdf96e63fdb2a46ff53
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-07 22:12:36 +0530

    Update vlog_des_3.js

--- static/vlog_des_3.js
+++ static/vlog_des_3.js
@@ -68,7 +68,24 @@ function addCode() {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", `/@${document.getElementById("username").innerText}/vlog/publish`, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader('X-CSRFToken', '{{ csrf_token }}');
+    function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+xhr.setRequestHeader('X-CSRFToken', csrftoken);
+
     // Prepare the data
     const data = JSON.stringify({ content: editorContent });
 
