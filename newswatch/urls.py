0db98f1b0cc7f80e7e632c930b670a75722f108c
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-11 21:11:17 +0530

    Update urls.py

--- newswatch/urls.py
+++ newswatch/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('auth/callback/', google_callback, name='google_callback'),
     path("login/", include('app.urls')),
     path('app/@<str:username_2>/follow', include('app.urls')),
-    path('<str:para>', include('app.urls')),
+    path('top/<str:para>', include('app.urls')),
     path('app/view/name/', include('app.urls')),
     path('about',include('app.urls')),
     path('vlog/show/<str:vlog_id>', include('app.urls')),
