4e2ee7ae3e8f6fbb35ac6dc043f3602b822a6fd6
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-11 13:20:16 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -651,7 +651,7 @@ def login_view(request):
                 login(request, user)
                 return redirect('/')  # Replace '/' with your desired redirect page
             else:
-                return render(request, 'login.html')
+                return render(request, 'login.html', {'error': "Invalid email or password."})
         except User.DoesNotExist:
             return render(request, 'login.html', {'error': "Invalid email or password."})
 
