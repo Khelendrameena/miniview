e383ea6853d539d27f74c2972eb70b38881922c7
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-01-12 01:00:54 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -110,7 +110,7 @@ def custom_sitemap(request):
     # Add dynamic user profile URLs
     for user in users:
         url_elem = ET.SubElement(urlset, 'url')
-        ET.SubElement(url_elem, 'loc').text = f'https://miniview-uzfa.onrender.com/@{user.username}/'
+        ET.SubElement(url_elem, 'loc').text = f'https://miniview-uzfa.onrender.com/@{user.username}'
         ET.SubElement(url_elem, 'lastmod').text = '2025-01-11'
         ET.SubElement(url_elem, 'changefreq').text = 'daily'
         ET.SubElement(url_elem, 'priority').text = '0.7'
@@ -118,7 +118,7 @@ def custom_sitemap(request):
     # Add dynamic vlog URLs
     for vlog in vlogs:
         url_elem = ET.SubElement(urlset, 'url')
-        ET.SubElement(url_elem, 'loc').text = f'https://miniview-uzfa.onrender.com/vlog/show/{vlog.vlog_id}/'
+        ET.SubElement(url_elem, 'loc').text = f'https://miniview-uzfa.onrender.com/vlog/show/{vlog.vlog_id}'
         ET.SubElement(url_elem, 'lastmod').text = '2025-01-11'
         ET.SubElement(url_elem, 'changefreq').text = 'weekly'
         ET.SubElement(url_elem, 'priority').text = '0.6'
