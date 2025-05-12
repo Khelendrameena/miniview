bf661e2f1cc0cbcd7457fa7d9b2573d7296ff3dd
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2025-05-12 10:07:29 +0530

    Update settings.py

--- newswatch/settings.py
+++ newswatch/settings.py
@@ -163,7 +163,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'khelendra1112@gmail.com'
-EMAIL_HOST_PASSWORD = 'kxir tnrr offd yflq'
+EMAIL_HOST_PASSWORD = 'gczi peok vwnw pxac'
 
 
 STATICFILE_DIRS = [
