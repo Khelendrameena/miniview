51e875f0a3251119a6b965b5fce1fa260aaa3698
khelendrameena <khelendra1112@gmail.com>
2026-04-24 16:04:23 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,36 +1,5 @@
 from django.db import models
 
-
-class MyModel(models.Model):
-    id = models.CharField(
-        max_length=100,  # Adjust length as needed
-        primary_key=True,  # Make it the primary key
-        unique=True,  # Ensure the ID is unique
-    )
-    views = models.IntegerField(default=0)  # Views field
-    likes = models.IntegerField(default=0)  # Likes field
-
-    def __str__(self):
-        return f"ID: {self.id}, Views: {self.views}, Likes: {self.likes}"
-
-
-
-class comentconfig(models.Model):
-      mainid = id = models.CharField(
-        max_length=100,  # Adjust length as needed
-      )
-      id = id = models.CharField(
-        max_length=100,  # Adjust length as needed
-        primary_key=True,
-        unique=True
-      )
-      name = models.CharField(max_length=100)
-      mess = models.TextField()
-      like = models.IntegerField(default=0)
-
-      def __str__(self):
-        return f"{self.id}"
-
 class Profile(models.Model):
     profile_id = models.CharField(max_length=100, unique=True, primary_key=True, default="default_id")  # Default value
     profile_picture = models.CharField(max_length=1000, blank=True)
@@ -45,42 +14,4 @@ class Profile(models.Model):
     def __str__(self):
         return self.username
 
-class Vlog(models.Model):     
-    vlog_id = models.CharField(max_length=100, unique=True, primary_key=True, default="default_id")
-    thumbnail = models.CharField(max_length=400)
-    title = models.CharField(max_length=800)
-    description = models.TextField()
-    content_html = models.TextField()
-    user = models.CharField(max_length=100)
-    likes = models.IntegerField(default=0)  # True for like (1), False for dislike (0)
-    comment = models.IntegerField(default=0)  # True if commented, else False
-    views = models.IntegerField(default=0)  # True if viewed, else False
-    vlog_labels = models.CharField(max_length=100,default="null")
-    vlog_rate = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
-    date_posted = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-class DraftVlog(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    vlog_id = models.CharField(max_length=255)
-    thumbnail = models.CharField(max_length=255, null=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class UserReaction(models.Model):
-    vlog_id = models.CharField(max_length=255,unique=True)  # Unique identifier for the vlog
-    username = models.CharField(max_length=100)
-    follow = models.IntegerField(default=0)
-    follow_to = models.CharField(max_length=100,default='null')
-    like = models.IntegerField(default=0)  # True for like (1), False for dislike (0)
-    comment = models.IntegerField(default=0)  # True if commented, else False
-    views = models.IntegerField(default=0)  # True if viewed, else False
-    user_interest = models.CharField(max_length=200,default='null')
-    interest_rate = models.FloatField(default=0.0)  # Rating or interest percentage
-
-    def __str__(self):
-        return f"Reaction by {self.username} on vlog {self.vlog_id}"
 
--- newswatch/settings.py
+++ newswatch/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
     'allauth',
     'allauth.account',
     'allauth.socialaccount'
@@ -163,8 +162,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'khelendra1112@gmail.com'
-EMAIL_HOST_PASSWORD = 'gczi peok vwnw pxac'
+EMAIL_HOST_USER = 'khelendra1010@gmail.com'
+EMAIL_HOST_PASSWORD = 'exdm mlsd xebl jtwa'
 
 
 STATICFILE_DIRS = [
