e252a3616a90d24b356e0ed873673c5824d806b5
Khelendra meena <168931780+Khelendrameena@users.noreply.github.com>
2026-04-24 16:17:52 +0530

    Update settings.py

--- newswatch/settings.py
+++ newswatch/settings.py
@@ -162,8 +162,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'khelendra1010@gmail.com'
-EMAIL_HOST_PASSWORD = 'exdm mlsd xebl jtwa'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 STATICFILE_DIRS = [
